5738d2fb50cc3a6c027e71d2dcad19da203075af
Roman Kostenko <romen.kost.2332@gmail.com>
2022-02-18 15:52:27 +0200

    Moved youtube api key to the application.yaml.

--- src/main/java/com/kostenko/youtubeanalyticservice/config/YoutubeAnalyticConfiguration.java
+++ src/main/java/com/kostenko/youtubeanalyticservice/config/YoutubeAnalyticConfiguration.java
@@ -2,11 +2,9 @@ package com.kostenko.youtubeanalyticservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@PropertySource("classpath:.env")
 public class YoutubeAnalyticConfiguration {
     @Bean
     public RestTemplate getRestTemplate() {
